fc3bbfa093f8b264db998b31d6c64742a60adc17
Antonio <you@example.com>
2025-12-05 15:14:40 +0100

    en ex04 entrada y salida funcional

---
+++ ex04/main.cpp
@@ -0,0 +1,63 @@
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+std::string compose_filename(char* original)
+{
+	std::string filename(original);
+	filename.append(".replace");
+
+	return filename;
+}
+
+static std::string f_to_string(char* original)
+{
+	std::ifstream file(original);
+	if(!file)
+	{
+		std::cout <<"Fail open " <<original <<std::endl;
+		return "";
+	}
+
+	std::ostringstream input;
+	input << file.rdbuf();
+
+	std::string content = input.str();
+	return (content);
+
+}
+
+static void str_to_file(std::string content, std::string filename)
+{
+	std::ofstream output(filename);
+	output << content;
+}
+
+ft_replace(std::string& src, std::string occ, std::string subs);
+{
+	
+
+}
+
+int main(int argc, char **argv)
+{
+
+	if (argc =! 4)
+	{
+		std::cout <<"Use: ./SED <file> <s1> <s2>" <<std::endl;
+		return 1;
+	}
+
+	std::string content = f_to_string(argv[1]);
+	if(content.empty())
+		return 1;
+
+	ft_replace(content, argv[2], argv[3]);
+
+	std::string filename = compose_filename(argv[1]);
+	str_to_file(content, filename);
+
+
+
+}
